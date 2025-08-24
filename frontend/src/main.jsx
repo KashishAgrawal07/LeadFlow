cb278db080e0f8977fc0f0b42071690719697cbd
KashishAgrawal07 <agrawalkashish111@gmail.com>
2025-08-24 17:24:29 +0530

    error 321 resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>   {/* ✅ provides context for useAuth() */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
